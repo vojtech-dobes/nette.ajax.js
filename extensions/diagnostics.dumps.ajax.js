244ff42bc4913b88b86e516decb063672eb2893c
Vojtech Dobes <me@vojtechdobes.com>
2012-06-15 02:20:59 +0200

    Extensions: added 'diagnostics.dumps'

---
+++ extensions/diagnostics.dumps.ajax.js
@@ -0,0 +1,53 @@
+(function($, undefined) {
+
+/**
+ * Shows barDumps from Ajax request
+ */
+$.nette.ext('diagnostics.dumps', {
+	success: function (payload) {
+		if (!payload.netteDumps || payload.netteDumps.length == 0) return;
+
+		var $panel = this.getPanel(), $helper = $('<div>');
+		$(this.getTitle(++this.counter)).appendTo($panel);
+
+		$.each(payload.netteDumps, function (i, item) {
+			if (item.title) $('<h2>', {
+				text: title
+			}).appendTo($helper);
+			var $table = $('<table>').appendTo($helper), i = 0;
+			$.each(item.dump, function (key, dump) {
+				var $caption = $('<th>')
+				$table.append($('<tr>', {
+					'class': i++ % 2 ? 'nette-alt' : '', 
+					html: '<th>' + key + '</th><td>' + dump + '</td>'
+				}));
+			});
+		});
+		$helper.children().appendTo($panel);
+
+		this.notify();
+	}
+}, {
+	counter: 0,
+	getTitle: function (count) {
+		return '<h2>Ajax request #' + count + '</h2>';
+	},
+	getTab: function () {
+		if (!this.tab) this.tab = $('[rel=Nette-Diagnostics-DefaultBarPanel-4]');
+		return this.tab;
+	},
+	getPanel: function () {
+		if (!this.panel) this.panel = $('#nette-debug-panel-Nette-Diagnostics-DefaultBarPanel-4 .nette-DumpPanel');
+		return this.panel;	
+	},
+	notify: function () {
+		if (this.notified) return;
+		$('<strong>', {
+			text: ' [ajax!]',
+			css: { fontWeight: 'bold' } }
+		).appendTo(this.getTab());
+		this.notified = true;
+	}
+});
+
+})(jQuery);
