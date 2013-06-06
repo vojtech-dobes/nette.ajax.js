61ae5822922283869d75473555da4029f7d49773
Vojtěch Dobeš <me@vojtechdobes.com>
2013-06-07 01:30:20 +0300

    typo

--- extensions/confirm.ajax.js
+++ extensions/confirm.ajax.js
@@ -1,7 +1,7 @@
 (function($, undefined) {
 
 $.nette.ext({
-  before: function (xhr, settings) {
+	before: function (xhr, settings) {
 		var question = settings.nette.el.data('confirm');
 		if (question) {
 			return confirm(question);
